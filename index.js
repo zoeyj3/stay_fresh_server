521594f7baf348c9e58ef241701f2444ed7bd5a3
zoeyj <zoeyj8170@gmail.com>
2024-04-07 19:53:21 -0700

    add routes and get list, get individual, post function

--- index.js
+++ index.js
@@ -1,1 +1,87 @@
-console.log("server working")
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const fs = require("fs");
+const uniqid = require("uniqid");
+const cors = require("cors")
+
+
+app.use(express.json());
+app.use(cors());
+app.use("/static-files", express.static("public"));
+
+app.listen(8080, function() {
+    console.log("Server is running on port " + 8080);
+});
+
+
+
+//parse JSON file
+function readFullInventoryList(){
+    const inventoryList = fs.readFileSync('./data/inventory.json')
+    const parsedInventoryList = JSON.parse(inventoryList)
+    return parsedInventoryList
+}
+
+//get full list
+app.get("/inventory", (req, res) => {
+    res.json(readFullInventoryList());
+});
+
+//get individual inventory by id
+app.get("/inventory/:id", (req, res) => {
+    const parsedInventoryList = readFullInventoryList()
+    const individualInventory = parsedInventoryList.filter((item) => item.id == req.params.id)
+    // id validation check
+    const inventoryChecked = parsedInventoryList.find((item) => item.id == req.params.id)
+    if(!inventoryChecked){
+        return res.status(404).send('item not found');
+    }
+    res.json(individualInventory);
+});
+// get individual inventory by name
+app.get("")
+
+
+
+
+app.use((req, res, next) => {
+    console.log("Middleware running on router");
+    next();
+});
+
+// add inventory
+app.post("/add", (req, res) =>{
+    const newInventory ={
+        id:uniqid(),
+        name:req.body.name,
+        storing_place:req.body.storing_place,
+        create_time:req.body.create_time,
+        best_before:req.body.best_before,
+        servings:req.body.servings,
+        is_deleted:req.body.is_deleted
+    };
+    const newList = readFullInventoryList();
+    newList.push(newInventory);
+    fs.writeFileSync('./data/inventory.json',JSON.stringify(newList));
+    res.status(201).json(newList);
+})
+
+// update inventory
+app.put("/inventory/:id", (req, res) => {
+    const parsedInventoryList = readFullInventoryList()
+    let individualInventory = parsedInventoryList.filter((item) => item.id == req.params.id)
+    
+    // id validation check
+   
+    const inventoryChecked = parsedInventoryList.find((item) => item.id == req.params.id)
+    if(!inventoryChecked){
+        return res.status(404).send('item not found');
+    }
+    //replace the new item
+
+    const newList = readFullInventoryList();
+    newList.push(updateInventory);
+    fs.writeFileSync('./data/inventory.json',JSON.stringify(newList));
+    res.status(201).json(newList);
+});
